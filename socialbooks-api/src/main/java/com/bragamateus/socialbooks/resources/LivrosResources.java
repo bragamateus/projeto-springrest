dbe2eeba6409350cefc93f2f750e2621a38ab84a
Your Name <braga.mateus16@gmail.com>
2019-11-01 16:18:56 -0300

    Criando uma representação para o recurso livro

--- socialbooks-api/src/main/java/com/bragamateus/socialbooks/resources/LivrosResources.java
+++ socialbooks-api/src/main/java/com/bragamateus/socialbooks/resources/LivrosResources.java
@@ -1,15 +1,28 @@
 package com.bragamateus.socialbooks.resources;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.bragamateus.socialbooks.domain.Livro;
 
 @RestController
 public class LivrosResources {
 	
 	@RequestMapping(value = "/livros", method = RequestMethod.GET)
-	public String listar() {
-		return "Rest Aplicado, Git passo-a-passo";
+	public List<Livro> listar() {
+		
+		Livro livro1 = new Livro("Rest Applicado");
+		Livro livro2 = new Livro("Git passo-a-passo");
+		
+		Livro[] livros = {livro1, livro2};
+		
+		return Arrays.asList(livros);
+		
+		
 	}
 
 }
