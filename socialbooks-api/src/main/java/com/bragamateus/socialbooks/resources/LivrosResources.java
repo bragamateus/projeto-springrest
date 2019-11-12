72568784df86ce6835e54942361101174510cc81
Your Name <braga.mateus16@gmail.com>
2019-11-11 22:53:11 -0300

    Buscando um livro com o uso da anotação @PathVariable

--- socialbooks-api/src/main/java/com/bragamateus/socialbooks/resources/LivrosResources.java
+++ socialbooks-api/src/main/java/com/bragamateus/socialbooks/resources/LivrosResources.java
@@ -1,8 +1,10 @@
 package com.bragamateus.socialbooks.resources;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -27,6 +29,11 @@ public class LivrosResources {
 	@RequestMapping(method = RequestMethod.POST)
 	public void salvar(@RequestBody Livro livro) {
 		livrosRepository.save(livro);
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public Optional<Livro> buscar(@PathVariable Long id) {
+		return livrosRepository.findById(id);
 	}
 
 }
