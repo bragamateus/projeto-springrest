7a4021ddaa0f393f1a9fe89a4405d6145f7c6fc1
Your Name <braga.mateus16@gmail.com>
2019-11-12 22:52:24 -0300

    Tratamento correto das respostas HTTP 404 e 201

--- socialbooks-api/src/main/java/com/bragamateus/socialbooks/resources/LivrosResources.java
+++ socialbooks-api/src/main/java/com/bragamateus/socialbooks/resources/LivrosResources.java
@@ -1,14 +1,17 @@
 package com.bragamateus.socialbooks.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.bragamateus.socialbooks.domain.Livro;
 import com.bragamateus.socialbooks.repository.LivrosRepository;
@@ -27,13 +30,24 @@ public class LivrosResources {
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
-	public void salvar(@RequestBody Livro livro) {
-		livrosRepository.save(livro);
+	public ResponseEntity<Void> salvar(@RequestBody Livro livro) {
+		
+		livro = livrosRepository.save(livro);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(livro.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
 	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public Optional<Livro> buscar(@PathVariable Long id) {
-		return livrosRepository.findById(id);
+	public ResponseEntity<?> buscar(@PathVariable Long id) {
+		Optional<Livro> livro = livrosRepository.findById(id);
+		if (!livro.isPresent()) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		return ResponseEntity.ok(livro.get());
 	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
